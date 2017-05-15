760f4b5d3b055c1935cb7c4b9d1fb3058513163d
dperrymorrow <dperrymorrow@gmail.com>
2017-05-15 16:33:46 -0700

    own parser for entity and line numbers

--- gitTasks/commands.json.js
+++ gitTasks/commands.json.js
@@ -1,15 +1,18 @@
-'use strict';
-
-const dilem = '--';
+"use strict";
 
 module.exports = {
-  dilem,
-  formatFields: ['sha', 'subject', 'body', 'authorName', 'authorEmail', 'date'],
-  branchRemote: 'git branch --remotes',
-  branchLocal: 'git branch --list',
-  branchCurrent: 'git rev-parse --abbrev-ref HEAD',
-  status: 'git status --porcelain',
-  log: `git log --format='${dilem} %nsha: %H %nauthor: %an %nemail: %ae %ndata: %ar %nsubject: %s%nbody: %b'`,
-  fetch: 'git fetch --all',
-  show: `git show {0} --pretty=format:'{"commit": "%H", "author":"%aN <%aE>", "date": "%ad", "subject": "%s", "body": "%b"}</endJSON>'`,
+  lineDilem: "[l]",
+  entryDilem: "[e]",
+  formatFields: ["sha", "subject", "body", "authorName", "authorEmail", "date"],
+  branchRemote: "git branch --remotes",
+  branchLocal: "git branch --list",
+  branchCurrent: "git rev-parse --abbrev-ref HEAD",
+  status: "git status --porcelain",
+  log: "git log --format='[e]%n[l] sha: %H%n[l] author: %an %n[l] email: %ae %n[l] data: %ar %n[l] subject: %s%n[l] body: %b'",
+  fetch: "git fetch --all",
+  show: `git show {0} --pretty=format:'{"commit": "%H", "author":"%aN <%aE>", "date": "%ad", "subject": "%s", "body": "%b"}'`,
 };
+
+
+
+git show 1d67d06d9c67eaa804ff40b93a0b0e2968b310f1 --pretty=format:'%n[l] sha: %H%n[l] author: %an %n[l] email: %ae %n[l] data: %ar %n[l] subject: %s%n[l] body: %b'
