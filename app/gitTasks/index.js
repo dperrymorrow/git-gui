58a6c933d187457f1e346a2963ea110bf3105f77
dperrymorrow <dperrymorrow@gmail.com>
2017-05-19 15:02:07 -0700

    ,
    {1}

--- app/gitTasks/index.js
+++ app/gitTasks/index.js
@@ -4,10 +4,10 @@ const base = require("./base");
 const commands = require("./commands.json");
 
 module.exports = {
+  base,
   log: require("./log"),
   status: require("./status"),
   branch: require("./branch"),
-  base,
   show: require("./show"),
   commit(args) {
     return base.run(commands.commit, [args]).catch(console.error);
