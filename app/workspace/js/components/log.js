1c4766e706293cdd075bb046272cca299feb45de
dperrymorrow <dperrymorrow@gmail.com>
2017-05-23 10:02:42 -0700

    styling the diff

--- app/workspace/js/components/log.js
+++ app/workspace/js/components/log.js
@@ -4,9 +4,14 @@ const dif2html = require("diff2html").Diff2Html;
 
 module.exports = {
   template: `
-    <div>
-      <pre v-if="hasEntries">{{ $store.state.log }}</pre>
-      <span v-else>no log entries...</span>
+    <div class="row">
+      <div class="col-xs-4">
+        <pre v-if="hasEntries">{{ $store.state.log }}</pre>
+        <span v-else>no log entries...</span>
+      </div>
+      <div class="col-xs-8">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
     </div>
   `,
 
--- app/workspace/js/components/status.js
+++ app/workspace/js/components/status.js
@@ -8,24 +8,28 @@ module.exports = {
   template: `
     <div class="row status">
 
-      <div class="col-md-4 col-xs-12 file-list">
-        <div class="commit-box" v-if="$store.getters.isDirty">
-          <input type="text" placeholder="subject of your commit" v-model="subject"/>
-          <textarea placeholder="optional: body of your commit" v-model="body"/></textarea>
-          <button class="block" @click.prevent="commit">
-            <i class="octicon octicon-git-commit"></i>
-            Commit to {{ $store.state.currentBranch }}
-          </button>
-        </div>
+      <div class="col-md-4 col-xs-12">
 
-        <div v-if="$store.getters.isDirty">
-          <div class="file" @click="showDiff(file.file)" :class="file.status.toLowerCase()" v-for="file in $store.state.status">
-            <span class="badge" :class="file.status.toLowerCase()">{{ file.status.charAt(0) }}</span>
-            {{ file.file }}</td>
+        <div class="file-list">
+
+          <div v-if="$store.getters.isDirty">
+            <div class="file" @click="showDiff(file.file)" :class="file.status.toLowerCase()" v-for="file in $store.state.status">
+              <span class="badge" :class="file.status.toLowerCase()">{{ file.status.charAt(0) }}</span>
+              {{ file.file }}</td>
+            </div>
           </div>
-        </div>
 
-        <span v-else>Clean, no changes...</span>
+          <div class="commit-box" v-if="$store.getters.isDirty">
+            <input type="text" placeholder="subject of your commit" v-model="subject"/>
+            <textarea placeholder="optional: body of your commit" v-model="body"/></textarea>
+            <button class="block" @click.prevent="commit">
+              <i class="octicon octicon-git-commit"></i>
+              Commit to {{ $store.state.currentBranch }}
+            </button>
+          </div>
+
+          <span v-else>Clean, no changes...</span>
+        </div>
 
       </div>
 
